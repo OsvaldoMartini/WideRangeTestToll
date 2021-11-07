533514a8a95c7203f6654d5135bb1a6a46e7bea6
Osvaldo Martini <osvaldo.martini@gmail.com>
2021-11-07 13:29:49 +0000

    Clean Up Previous Backend

--- Application/Interfaces/IEmailSender.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace Application.Interfaces
-{
-    public interface IEmailSender
-    {
-        Task SendEmailAsync(string userEmail, string emailSubject, string message);
-    }
-}
--- Infrastructure/Email/SendGridSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Infrastructure.Email
-{
-    public class SendGridSettings
-    {
-        public string NickName { get; set; }
-        public string ApiKey { get; set; }
-        public string ApiSecret { get; set; }
-    }
-}
