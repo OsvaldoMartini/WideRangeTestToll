b69fac7f5b9bbfb7160b7b16a9c91b6fe4760bab
Osvaldo Martini <osvaldo.martini@gmail.com>
2021-11-07 14:59:38 +0000

    Clean Up and Changes to Use Only TypeScript Templates

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,30 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
-import {createBrowserHistory} from 'history';
-import 'react-toastify/dist/ReactToastify.min.css';
-import 'react-widgets/dist/css/react-widgets.css';
-import 'semantic-ui-css/semantic.min.css'
-import './app/layout/styles.css';
-import App from './app/layout/App';
-import * as serviceWorker from './serviceWorker';
-import ScrollToTop from './app/layout/ScrollToTop';
-import dateFnsLocalizer from 'react-widgets-date-fns';
-
-dateFnsLocalizer();
-
-export const history = createBrowserHistory();
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Router history={history}>
-    <ScrollToTop>
-      <App />
-    </ScrollToTop>
-  </Router>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
