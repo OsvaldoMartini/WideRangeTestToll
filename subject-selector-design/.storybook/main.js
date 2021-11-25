981bdc0cd30bc735322db462640e4465a4fbe619
Osvaldo Martini <OSMA1@hscic.gov.uk>
2021-11-25 16:01:36 +0000

    depe

--- subject-selector-design/.storybook/main.js
+++ subject-selector-design/.storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ["../stories/**/*.stories.@(ts|tsx|js|jsx)"],
   addons: [
-    "storybook-zeplin",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
